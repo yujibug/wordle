8e0b7554eec6bebb855b195cd0dd12e14d7d69e0
gimyuji476@gmail.com <gimyuji476@gmail.com>
2022-03-22 06:00:35 +0900

    실제 키보드 연동 기능 구현

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useEffect, useCallback, useContext } from 'react';
+import { AppContext } from './App';
 import Key from './Key';
 
 function Keyboard() {
+  const { onEnter, onDelete, onSelectLetter } = useContext(AppContext);
   const keys1 = ['ㅂ', 'ㅈ', 'ㄷ', 'ㄱ', 'ㅅ', 'ㅛ', 'ㅕ', 'ㅑ'];
   const keys2 = ['ㅁ', 'ㄴ', 'ㅇ', 'ㄹ', 'ㅎ', 'ㅗ', 'ㅓ', 'ㅏ', 'ㅣ'];
   const keys3 = ['ㅋ', 'ㅌ', 'ㅊ', 'ㅍ', 'ㅠ', 'ㅜ', 'ㅡ'];
 
+  const keysEng1 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i'];
+  const keysEng2 = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+  const keysEng3 = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+
+  const handleKeyboard = useCallback((event) => {
+    if (event.key === 'Enter') {
+      onEnter();
+    } else if (event.key === 'Backspace') {
+      onDelete();
+    } else {
+      for (let i = 0; i < 7; i++) {
+        if (event.key.toLowerCase() === keysEng1[i]) {
+          onSelectLetter(keys1[i]);
+        }
+      }
+      for (let i = 0; i < 8; i++) {
+        if (event.key.toLowerCase() === keysEng2[i]) {
+          onSelectLetter(keys2[i]);
+        }
+      }
+      for (let i = 0; i < 6; i++) {
+        if (event.key.toLowerCase() === keysEng3[i]) {
+          onSelectLetter(keys3[i]);
+        }
+      }
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyboard);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div className='keyboard'>
+    <div className='keyboard' onKeyDown={handleKeyboard}>
       <div className='line1'>
         {keys1.map((key, index) => {
           return <Key keyVal={key} key={index}></Key>;
